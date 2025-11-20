235ae8fef5d5c82ffa09ff9b3333422cfcd9fbae
Daniel Petti <djpetti@gmail.com>
2025-11-20 14:33:25 -0500

    Improve names of uploaded videos.
    These names will now directly correspond to the camera that they came from,
    instead of being arbitrary integers.

--- scripts/extract_videos.py
+++ scripts/extract_videos.py
@@ -43,7 +43,7 @@ def _split_bag(
             it remains responsive to the user.
 
     """
-    out_file_base = output_dir / "camera_"
+    out_file_base = output_dir / "mars_"
 
     # Start the node.
     launcher = LaunchManager(
@@ -245,7 +245,7 @@ def process_bag(
         )
 
         # Find extracted video files.
-        video_files = set(video_dir.glob("camera_*"))
+        video_files = set(video_dir.glob("mars_camera*"))
         # Ignore timestamps.
         video_files -= set(video_dir.glob("*.txt"))
         video_files = sorted(video_files)
@@ -260,7 +260,7 @@ def process_bag(
 
         # Transcode those videos.
         for i, video_file in enumerate(video_files):
-            output_file = output_base.parent / f"{output_base.name}_cam{i}.mp4"
+            output_file = output_base.parent / f"{video_file.stem}.mp4"
             video_output_files.append(output_file)
             decoder = decoders[video_file.suffix]
             try:
